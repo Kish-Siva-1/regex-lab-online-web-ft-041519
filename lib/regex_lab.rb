cbef135a4b97041762e36a68fa92444e73dc3f2c
Kish Siva <rodes88@msn.com>
2019-04-18 17:56:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   text2 = text.split(" ")
-  text2.grep(/\w+[un]{2}*[ing]{3}/)
+  text2.grep(/\w+["un"]{2}*["ing"]{3}/)
 end
 
 def words_five_letters_long(text)
